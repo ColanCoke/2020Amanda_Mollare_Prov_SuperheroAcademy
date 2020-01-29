6ee50813043fa11935a34b4af640173856a28a51
ColanCoke <amanda.mollare@elev.ga.ntig.se>
2020-01-29 14:19:21 +0100

    Började skapa klasser

---
+++ Superhero_Academy_Amanda_TE17A/Superhero_Academy_Amanda_TE17A/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero_Academy_Amanda_TE17A
+{
+    class Hero //Arvklass som innehåller diverse attribut som alla hjältetyper ska ha.
+    {
+        protected int speed = 0;
+
+        protected int strength = 0;
+
+        protected int heroism = 0;
+
+        protected int kindness = 0;
+    }
+}
